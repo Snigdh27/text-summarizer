4be7d62b4d8e9eddcbfb15fc0edaf2024d36d4a0
Snigdh Kulshreshtha <74950528+Snigdh27@users.noreply.github.com>
2023-05-08 19:14:42 +0530

    Add files via upload

---
+++ static/model.py
@@ -0,0 +1,4 @@
+import tensorflow
+
+encoder_model = tensorflow.keras.models.load_model(r'd:\Users\Lenovo\Downloads\encoder_model\encoder_model')
+# decoder_model = tensorflow.keras.models.load_model(r'd:\Users\Lenovo\Downloads\decoder_model\decoder_model')
